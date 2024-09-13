37490fbbe059f3e14a6c66b8db065f85a9c3371b
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-09-13 18:02:00 -0300

    feat: add candidate body validation

--- src/main/java/com/thiagodff/job_manager/modules/candidates/CandidateEntity.java
+++ src/main/java/com/thiagodff/job_manager/modules/candidates/CandidateEntity.java
@@ -2,6 +2,10 @@ package com.thiagodff.job_manager.modules.candidates;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).+", message = "Username must not contain spaces")
   private String username;
+
+  @Email(message = "Email should be valid")
   private String email;
+
+  @Length(min = 6, max = 100, message = "Password should have at least 6 characters and at most 100")
   private String password;
   private String description;
   private String curriculum;
--- src/main/java/com/thiagodff/job_manager/modules/candidates/controllers/CandidateController.java
+++ src/main/java/com/thiagodff/job_manager/modules/candidates/controllers/CandidateController.java
@@ -5,16 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagodff.job_manager.modules.candidates.CandidateEntity;
 
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/candidates")
+@RequestMapping("/candidate")
 public class CandidateController {
 
   @PostMapping("/")
-  public void create(@RequestBody CandidateEntity candidate) {
+  public void create(@Valid @RequestBody CandidateEntity candidate) {
       System.out.println("Creating candidate: " + candidate.getEmail());
   }
 }
