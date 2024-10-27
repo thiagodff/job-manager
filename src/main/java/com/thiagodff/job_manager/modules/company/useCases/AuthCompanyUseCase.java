4ed819c4f04e027ff6fcee90836f9a7d9cd61c8a
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-10-27 18:23:22 -0300

    feat: create security filter middleware

--- src/main/java/com/thiagodff/job_manager/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/thiagodff/job_manager/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package com.thiagodff.job_manager.modules.company.useCases;
 
+import java.time.Instant;
+import java.time.Duration;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +44,7 @@ public class AuthCompanyUseCase {
     Algorithm algorithm = Algorithm.HMAC256(secreteKey);
     var token = JWT.create()
       .withIssuer("quare_software")
+      .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
       .withSubject(company.getId().toString())
       .sign(algorithm);
 
--- src/main/java/com/thiagodff/job_manager/security/SecurityConfig.java
+++ src/main/java/com/thiagodff/job_manager/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.thiagodff.job_manager.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration // queremos que seja uma classe de config que o spring gerencie
 public class SecurityConfig {
+
+  @Autowired
+  private SecurityFilter securityFilter;
 
   // avisa ao spring para sobrescrever o método já implementado pelo spring security
   @Bean
@@ -19,7 +24,9 @@ public class SecurityConfig {
           .requestMatchers("/company/").permitAll()
           .requestMatchers("/auth/company").permitAll();
         auth.anyRequest().authenticated();
-      });
+      })
+      // um filter/middleware/interceptor para todas as requisições
+      .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
     return http.build();
   }
 
