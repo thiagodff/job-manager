860fde045b115fe3a743be7f997d813a4822382f
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-11-08 01:10:16 -0300

    chore: define create job dto

--- src/main/java/com/thiagodff/job_manager/modules/company/controllers/JobController.java
+++ src/main/java/com/thiagodff/job_manager/modules/company/controllers/JobController.java
@@ -1,14 +1,18 @@
 package com.thiagodff.job_manager.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thiagodff.job_manager.modules.company.dto.CreateJobDTO;
 import com.thiagodff.job_manager.modules.company.entities.JobEntity;
 import com.thiagodff.job_manager.modules.company.useCases.CreateJobUseCase;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -19,8 +23,19 @@ public class JobController {
   private CreateJobUseCase createJobUseCase;
 
   @PostMapping("/")
-  public void create(@Valid @RequestBody JobEntity jobEntity) {
-    this.createJobUseCase.execute(jobEntity);
+  public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+    var companyId = request.getAttribute("company_id");
+
+    var jobEntity = JobEntity.builder()
+      .benefits(createJobDTO.getBenefits())
+      .description(createJobDTO.getDescription())
+      .level(createJobDTO.getLevel())
+      .companyId(UUID.fromString(companyId.toString()))
+      .build();
+
+    jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
+
+    return this.createJobUseCase.execute(jobEntity);
   }
 
 }
---
+++ src/main/java/com/thiagodff/job_manager/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,12 @@
+package com.thiagodff.job_manager.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+  private String description;
+  private String benefits;
+  private String level;
+
+}
