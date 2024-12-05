a1b7d44468a73ce1377f205d8114a17395c944b2
Thiago Dornelles <thiago.fdornelles@gmail.com>
2024-12-04 22:24:32 -0300

    feat: config swagger

--- src/main/java/com/thiagodff/job_manager/JobManagerApplication.java
+++ src/main/java/com/thiagodff/job_manager/JobManagerApplication.java
@@ -3,7 +3,17 @@ package com.thiagodff.job_manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Job Manager",
+		description = "API responsible for managing jobs",
+		version = "1.0.0"
+	)
+)
 public class JobManagerApplication {
 
 	public static void main(String[] args) {
